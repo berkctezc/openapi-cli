c13ad80d2046ad480907fc9c3af93962001c5405
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2024-01-27 08:56:19 +0300

    rename

--- Library/Constants.cs
+++ Library/Constants.cs
@@ -2,7 +2,7 @@ namespace Library;
 
 public static class Constants
 {
-    public const string ClientName = "OpenApiCliBulk";
+    public const string ClientName = "OpenSubtitlesCliBulk";
     public const string BaseUrl = "https://api.opensubtitles.com/api/v1";
     public const string ApiKey = "";
     public const string UserAgent = "openapi-bulk-net v0.1";
